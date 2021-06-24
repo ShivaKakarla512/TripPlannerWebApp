46c496307eee5f5e20a2b9aa3a634f8e2789dc0a
Shiva Kakarla <shiva.kakarla512@gmail.com>
2021-06-24 21:36:34 +0530

    trying

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from os import environ
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my_secret"
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL?sslmode=require') or 'sqlite:///my_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL?sslmode=require')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
