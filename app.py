8cf922d18ccc28e54d90cf77e75f33c0d7ef01ed
Shiva Kakarla <shiva.kakarla512@gmail.com>
2021-06-24 20:57:33 +0530

    postgres issue fixed?

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config["SECRET_KEY"] = "my_secret"
 uri = environ.get("DATABASE_URL") or 'sqlite:///my_database.db'
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-SQLALCHEMY_DATABASE_URI = uri
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
