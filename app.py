51073b9ef9836d4ba9e1fa4db5fdf240fcdefff2
Shiva Kakarla <shiva.kakarla512@gmail.com>
2021-06-24 20:46:18 +0530

    postgres change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from os import environ
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my_secret"
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL?sslmode=require') or 'sqlite:///my_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://husysxlukvwsek:5ca6d009c546bc0d3e725fc77008899754588d5674150a4e771c49ff07b9801e@ec2-34-193-112-164.compute-1.amazonaws.com:5432/daasknvfvi0nth" or 'sqlite:///my_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
