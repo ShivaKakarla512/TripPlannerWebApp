c583940ee7b410e90443b2ca50628c1d4f613efe
Shiva Kakarla <shiva.kakarla512@gmail.com>
2021-06-24 21:02:19 +0530

    postgres issue fixed

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ from os import environ
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my_secret"
-
-uri = environ.get("DATABASE_URL") or 'sqlite:///my_database.db'
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL?sslmode=require') or 'sqlite:///my_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
