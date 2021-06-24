f4c0494e693121e541bc5e20799e72483645245c
Shiva Kakarla <shiva.kakarla512@gmail.com>
2021-06-24 19:58:31 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from os import environ
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "my_secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL?sslmode=require') or 'sqlite:///my_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+import routes, models
---
+++ models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+  
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+ 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password) 
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(140))
+    country = db.Column(db.String(140))
+    description = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.description)
+
+# db.create_all()
