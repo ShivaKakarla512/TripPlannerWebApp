1c492454fd1ce0b6fb31f33e65912850fc3e9cd3
Shiva Kakarla <shiva.kakarla512@gmail.com>
2021-07-01 19:44:37 +0530

    styling the login page

--- static/main.js
+++ static/main.js
@@ -1,3 +1,26 @@
+/*===== FOCUS =====*/
+const inputs = document.querySelectorAll(".form__input")
+
+/*=== Add focus ===*/
+function addfocus(){
+    let parent = this.parentNode.parentNode
+    parent.classList.add("focus")
+}
+
+/*=== Remove focus ===*/
+function remfocus(){
+    let parent = this.parentNode.parentNode
+    if(this.value == ""){
+        parent.classList.remove("focus")
+    }
+}
+
+/*=== To call function===*/
+inputs.forEach(input=>{
+    input.addEventListener("focus", addfocus)
+    input.addEventListener("blur", remfocus)
+})
+
 /*===== MENU SHOW Y HIDDEN =====*/
 const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('nav-toggle'),
