fe08d80f771c021c1d3da35becc38b1863a8d45c
Shiva Kakarla <shiva.kakarla512@gmail.com>
2021-07-07 19:58:08 +0530

    locked the orientation again again

--- static/main.js
+++ static/main.js
@@ -1,4 +1,34 @@
-// screen.orientation.lock();
+function lock (orientation) {
+// Go into full screen first
+if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+} else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+} else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+} else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+}
+
+// Then lock orientation
+screen.orientation.lock(orientation);
+}
+
+function unlock () {
+// Unlock orientation
+screen.orientation.unlock();
+
+// Exit full screen
+if (document.exitFullscreen) {
+    document.exitFullscreen();
+} else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+} else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+} else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+}
+}
 /*===== FOCUS =====*/
 const inputs = document.querySelectorAll(".form__input")
 
